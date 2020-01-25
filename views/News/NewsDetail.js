7404d4cb11aa57ff18a9af1c66b42bb11a2c32f4
invesible-gude <59083810+invesible-gude@users.noreply.github.com>
2020-01-25 16:11:14 +0700

    Update NewDetail
    edit color

--- views/News/NewsDetail.js
+++ views/News/NewsDetail.js
@@ -75,7 +75,7 @@ export class NewsDetail extends React.Component {
                         >
                         </Image>
                         <View style={{ padding: 15, }}>
-                            <Text style={[ styles.text_font, { fontSize: 20, color: '#ff9900', }]}>
+                            <Text style={[ styles.text_font, { fontSize: 20, color: '#723332', }]}>
                                 {this.state.news.news_title} <Text style={[ styles.text_font, { fontSize: 14, color: 'grey', }]}>{this.state.news.news_date}</Text>
                             </Text>
                         </View>
@@ -95,7 +95,7 @@ export class NewsDetail extends React.Component {
         }
 
         return (
-            <ScrollView style={{ backgroundColor: '#010001', }}>
+            <ScrollView style={{ backgroundColor: '#ffccc8', }}>
                 <StatusBar hidden={true} />
                 {display_data} 
             </ScrollView>
@@ -107,6 +107,7 @@ const styles = StyleSheet.create({
 	text_font: {
 		fontFamily: 'Kanit-Regular',
 		fontSize: 16,
-        color: '#fff',
+        color: '#ce4448',
     },
+   
 });
