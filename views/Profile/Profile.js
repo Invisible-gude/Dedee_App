60dc22b324c23dc400a1987f4d9f8718518e21bc
invesible-gude <59083810+invesible-gude@users.noreply.github.com>
2020-01-25 11:58:56 +0700

    Update Profile
    Edit color header content

--- views/Profile/Profile.js
+++ views/Profile/Profile.js
@@ -124,25 +124,25 @@ export default class Profile extends React.Component {
                         <TouchableOpacity
                             style={{
                                 alignSelf: 'center',
-                                backgroundColor: '#fff',
+                                backgroundColor: '#723332',
                                 width: 140,
                                 padding: 8,
                                 marginTop: 16,
                                 borderRadius: 30,
                             }}
                             onPress={() => this._confirmLogout()}>
-                            <Text style={[ styles.text_font, { alignSelf: 'center', color: '#000', }]}>
+                            <Text style={[ styles.text_font, { alignSelf: 'center', color: '#fff' }]}>
                                 ลงชื่อออก
                             </Text>
                         </TouchableOpacity>
-                        
+
                     </View>
                 );
             }
         }
 
         return (
-            <ScrollView style={{ backgroundColor: '#4A2102', }}>
+            <ScrollView style={{ backgroundColor: '#f4c2c2', }}>
                  <ImageBackground 
                         resizeMode='cover' 
                         source={require('../../images/bghead.png')} 
@@ -157,7 +157,7 @@ export default class Profile extends React.Component {
 const styles = StyleSheet.create({
 	text_font: {
 		fontSize: 16,
-        color: '#fff',
+        color: '#ef6161',
     },
     profile_frame: {
         width: 120, 
@@ -166,7 +166,7 @@ const styles = StyleSheet.create({
         marginTop: 28,
         marginBottom: 28,
         alignSelf: 'center', 
-        backgroundColor: '#25aae1',
+        backgroundColor: '#ef6161',
         borderRadius: 60
     },
     profile_image: {
